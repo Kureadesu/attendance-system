c0244caaf4f923b224e39644b20fa60ec5a59dbb
Kureadesu <118508974+Kureadesu@users.noreply.github.com>
2025-11-03 00:51:55 +0800

    fixed imports

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,8 +1,8 @@
 // models/index.js
 import sequelize from '../config/database.js';
-import Student from './student.js';
-import Subject from './subject.js';
-import Attendance from './attendance.js'; // Add this
+import Student from './Student.js';
+import Subject from './Subject.js';
+import Attendance from './Attendance.js'; // Add this
 
 // Define associations
 Student.hasMany(Attendance, { foreignKey: 'student_number' });
